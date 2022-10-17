b3a476b72572a3fffdcfdac0f48bef15649678db
Alexey <slow.net@mail.ru>
2022-10-17 12:26:06 +0300

    Решение первой задачи семинара 1

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine(" Введите первое число");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" Введите второе число");
+
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int max;
+if (number1 > number2)
+{
+    max = number1;
+    Console.WriteLine("max = " + max);
+}
+else if (number1 < number2)
+{
+    max = number2;
+    Console.WriteLine("max = " + max);
+}
+else
+
+Console.WriteLine("Числа равны");
+
+
