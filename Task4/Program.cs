e0bcce45406a391f879fbcb4f47fb680a9f008e6
Alexey <slow.net@mail.ru>
2022-10-17 13:31:43 +0300

    Решение четвёртой задачи семинара 1

---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine(" Введите число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+
+int even;
+
+while (count <= number)
+{
+    if (count % 2 == 0)
+    {
+        even=count;
+        Console.Write(even + ",");
+    }
+    count++;
+}
